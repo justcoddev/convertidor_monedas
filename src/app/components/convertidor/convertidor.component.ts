a205e7ed87d82b5adc1f72481f20b8713f5a04b2
justcoddev <edisonpaulcrz@gmail.com>
2022-10-29 16:33:10 -0500

    event binding, ngIf, pipe

--- src/app/components/convertidor/convertidor.component.ts
+++ src/app/components/convertidor/convertidor.component.ts
@@ -11,11 +11,50 @@ export class ConvertidorComponent implements OnInit {
   quiero = 'EUR';
   total = 0;
 
-  monedas : string[]=['USD','EUR', 'LIBRA']
+  monedas: string[] = ['USD', 'EUR', 'LIBRA']
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  convertir() {
 
+    switch (this.tengo) {
+      case 'USD':
+        if (this.quiero === 'USD') {
+          this.total = this.cantidad;
+        }
+        if (this.quiero === 'EUR') {
+          this.total = this.cantidad * 1
+        }
+        if (this.quiero === 'LIBRA') {
+          this.total = this.cantidad * 0.86
+        }
+        break;
+      case 'EUR':
+        if (this.quiero === 'USD') {
+          this.total = this.cantidad * 1;
+        }
+        if (this.quiero === 'EUR') {
+          this.total = this.cantidad;
+        }
+        if (this.quiero === 'LIBRA') {
+          this.total = this.cantidad * 0.86
+        }
+        break;
+      case 'LIBRA':
+        if (this.quiero === 'USD') {
+          this.total = this.cantidad * 1.16;
+        }
+        if (this.quiero === 'EUR') {
+          this.total = this.cantidad * 1.17;
+        }
+        if (this.quiero === 'LIBRA') {
+          this.total = this.cantidad;
+        }
+        break;
+      default:
+        break;
+    }
+  }
 }
